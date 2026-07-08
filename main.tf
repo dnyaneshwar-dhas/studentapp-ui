7c0f2751945db88a71378e5c0fbebe56cfacb368
mayurpandit25 <panditmayur888@gmail.com>
2026-07-08 18:51:48 +0530

    modified

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_vpc" "default" { // Fetch default VPC
 
 variable "cluster_name" { // Variable for cluster name
   type    = string
-  default = "my-cluster-student"
+  default = "my-cluster"
 }
 
 data "aws_subnets" "default" { // Fetch  all subnets in the default VPC
